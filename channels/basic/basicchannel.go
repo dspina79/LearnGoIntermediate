0b249b2c63126aae32df9e08f0704d49b6d1d045
Dave Spina <dspina79@gmail.com>
2021-04-15 20:48:16 -0400

    Added diffent type and provided int passing channel

--- channels/basic/basicchannel.go
+++ channels/basic/basicchannel.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
+func square(c chan int, num int) {
+	c <- (num * num)
+}
+
 func main() {
 	strChannel := make(chan string)
+	intChannel := make(chan int)
+
+	go square(intChannel, 13)
+	go square(intChannel, 14)
+
+	topStack := <-intChannel
+	bottomStack := <-intChannel
+
+	fmt.Println("Top: ", topStack, " Bottom: ", bottomStack)
 
 	for i := 0; i < 5; i++ {
 		go func() { strChannel <- ("sample message " + fmt.Sprint(i)) }()
