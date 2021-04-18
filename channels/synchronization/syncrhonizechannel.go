22f8c6d26dcfc5697860779bef270508a47be785
Dave Spina <dspina79@gmail.com>
2021-04-17 23:15:27 -0400

    Added second channel for synchornization testing

--- channels/synchronization/syncrhonizechannel.go
+++ channels/synchronization/syncrhonizechannel.go
@@ -13,9 +13,20 @@ func executor(c chan bool) {
 	c <- true // without this, the system will deadlock
 }
 
+func printNumbers(c chan bool) {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Running number %d\n", i)
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	c <- true
+}
+
 func main() {
 	exchan := make(chan bool, 1)
+	numChan := make(chan bool, 1)
 	go executor(exchan)
-
+	go printNumbers(numChan)
+	<-numChan
 	<-exchan
 }
