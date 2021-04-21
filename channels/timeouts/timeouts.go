eea6e9058a9b20ec3b1094f41bbc3ff6cd40e51e
Dave Spina <dspina79@gmail.com>
2021-04-20 20:54:43 -0400

    Provided timeout examples

---
+++ channels/timeouts/timeouts.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func addMessage(c chan<- string, msg string, delay float64) {
+	time.Sleep(time.Second * time.Duration(delay))
+	c <- msg
+}
+
+func main() {
+	c1 := make(chan string, 1)
+	c2 := make(chan string, 1)
+
+	go addMessage(c1, "first message", 2)
+
+	select {
+	case res1 := <-c1:
+		fmt.Println(res1)
+	case <-time.After(time.Second * 3):
+		fmt.Println("Error: Timeout")
+	}
+
+	go addMessage(c2, "second message", 4)
+
+	select {
+	case res2 := <-c2:
+		fmt.Println(res2)
+	case <-time.After(time.Second * 3):
+		fmt.Println("Error: Timeout")
+	}
+}
