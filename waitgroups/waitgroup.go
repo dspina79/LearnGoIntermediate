5194aeca3b9c9e29ea1edf6999c88111b02bcf61
Dave Spina <dspina79@gmail.com>
2021-04-25 20:42:41 -0400

    Added another wait function on the wait group

--- waitgroups/waitgroup.go
+++ waitgroups/waitgroup.go
@@ -7,10 +7,17 @@ import (
 )
 
 func worker(id int, wg *sync.WaitGroup) {
-	defer wg.Done()
 	fmt.Printf("Worker %d starting...\n", id)
 	time.Sleep(time.Second)
 	fmt.Printf("Worker %d DONE!\n", id)
+}
+
+func otherWorker(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Calculating the square of %d\n", id)
+	time.Sleep(1500 * time.Millisecond)
+	fmt.Printf("The square of %d is %d\n", id, id*id)
 }
 
 func main() {
@@ -18,6 +25,7 @@ func main() {
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go worker(i, &wg)
+		go otherWorker(i, &wg)
 	}
 
 	wg.Wait()
