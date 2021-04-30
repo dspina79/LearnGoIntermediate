5db3a2740e1ce566989dbb3035fcb237bc3d9549
Dave Spina <dspina79@gmail.com>
2021-04-30 10:18:38 -0400

    Implemented sorting function by last char

--- sorting/byfunctions/sortbyfunctiong.go
+++ sorting/byfunctions/sortbyfunctiong.go
@@ -3,9 +3,25 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strings"
 )
 
+type byLastChar []string
 type sArr []string
+
+func (s byLastChar) Len() int {
+	return len(s)
+}
+
+func (s byLastChar) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byLastChar) Less(i, j int) bool {
+	si := strings.ToLower(strings.Split(s[i], "")[len(s[i])-1])
+	sj := strings.ToLower(strings.Split(s[j], "")[len(s[j])-1])
+	return si < sj
+}
 
 func (s sArr) Len() int {
 	return len(s)
@@ -21,6 +37,9 @@ func (s sArr) Less(i, j int) bool {
 
 func main() {
 	names := []string{"Dave", "Larry", "Michelle", "Bo", "Fortinbras", "Leu", "Lou", "Liu"}
+	names2 := []string{"Dave", "Larry", "Michelle", "Bo", "Fortinbras", "Leu", "Lou", "Liu"}
 	sort.Sort(sArr(names))
-	fmt.Println("Sorted: ", names)
+	sort.Sort(byLastChar(names2))
+	fmt.Println("Sorted by Length: ", names)
+	fmt.Println("Sorted by Last Char: ", names2)
 }
