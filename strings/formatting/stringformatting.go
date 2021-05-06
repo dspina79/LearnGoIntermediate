f38a2700664148035d7fbe49473894fa65b295ce
Dave Spina <dspina79@gmail.com>
2021-05-05 21:26:02 -0400

    String formatting examples

---
+++ strings/formatting/stringformatting.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type point struct {
+	x int
+	y int
+}
+
+func main() {
+
+	p := point{x: 1, y: 2}
+	fmt.Println("Basic Print", p)
+	fmt.Printf("v %v\n", p)
+	fmt.Printf("+v %+v\n", p)
+	fmt.Printf("#v %#v\n", p)
+
+	fmt.Printf("T %T\n", p)
+	fmt.Printf("t %t\n", true)
+
+	fmt.Printf("d (digit) %d\n", 123)
+	fmt.Printf("b (byte) %b\n", 123)
+	fmt.Printf("f (float) %f\n", 123.23)
+	fmt.Printf("x (hex) %x\n", 123)
+
+	fmt.Printf("c (char) %c\n", 68)
+
+	fmt.Printf("e %e\n", 12300000.00)
+	fmt.Printf("E %E\n", 12300000.00)
+
+	fmt.Fprintf(os.Stderr, "as %s\n", "error")
+
+}
