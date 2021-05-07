fac626098b658d462c45539ffaffdffa3f892e18
Dave Spina <dspina79@gmail.com>
2021-05-07 00:58:31 -0400

    Cleanup on regex

--- strings/reegularexpressions/regexexamples.go
+++ strings/reegularexpressions/regexexamples.go
@@ -8,15 +8,17 @@ import (
 func main() {
 	// uncompiled regex expression
 	toCompare := "watermelon"
-	match, _ := regexp.MatchString("w([a-z]+)melon", toCompare)
+	match, _ := regexp.MatchString("w([a-z]+)elon", toCompare)
 	fmt.Println("Did it match? ", match)
 
 	// compiled match
-	rCompiled, _ := regexp.Compile("w([a-z]+)melon")
+	rCompiled, _ := regexp.Compile("w([a-z]+)elon")
 	fmt.Println("Did it compile match? ", rCompiled.MatchString(toCompare))
 
 	sentenceToCompare := "There is a watermelon in my farm. I love watermelons."
 	fmt.Println("The sentence to start is: ", sentenceToCompare)
-	fmt.Println("FindASubstring Results:", rCompiled.FindStringSubmatch(sentenceToCompare))
-	fmt.Println("FindAllMatch Results: ", rCompiled.FindAllString(sentenceToCompare, -1))
+	fmt.Println("FindStringSubmatch Results:", rCompiled.FindStringSubmatch(sentenceToCompare))
+	fmt.Println("FindAllString Results: ", rCompiled.FindAllString(sentenceToCompare, -1))
+	fmt.Println("FindAllStringIndex Result:", rCompiled.FindAllStringIndex(sentenceToCompare, -1))
+	fmt.Println("FindStringSubmatchIndex Result:", rCompiled.FindStringSubmatchIndex(sentenceToCompare))
 }
